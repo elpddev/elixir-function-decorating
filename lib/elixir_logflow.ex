2521e05a4a49928b7146cd28483d61926a2c205e
eyal lapid <elpd.dev@gmail.com>
2016-03-14 19:54:05 +0200

    add macro param for disabling special def in module

--- lib/elixir_logflow.ex
+++ lib/elixir_logflow.ex
@@ -1,9 +1,6 @@
 defmodule ElixirLogflow do
-  defmacro __using__(_) do
-    quote do
-      import Kernel, except: [def: 2]
-      import ElixirLogflow
-    end
+  defmacro __using__(args_ast) do
+    do_using(args_ast)
   end
 
   defmacro def(fun_def_ast, opts_ast) do
@@ -13,9 +10,28 @@ defmodule ElixirLogflow do
   def do_def(fun_def_ast, opts_ast) do
     quote do
       Kernel.def unquote(fun_def_ast) do
-        IO.puts "Calling #{inspect unquote(Macro.escape(fun_def_ast))}"
+        do_log(unquote(Macro.escape(fun_def_ast)))
         unquote((opts_ast[:do]))
       end
+    end
+  end
+
+  def do_log(fun_def_ast_struct) do
+    IO.puts "Calling #{inspect fun_def_ast_struct}"
+  end
+
+  def do_using(args_ast) do
+    {args, []} = Code.eval_quoted(args_ast)
+    skip_log_flag = Keyword.get(args, :skip_log, false)
+
+    case skip_log_flag do
+      true ->
+        nil
+      false ->
+        quote do
+          import Kernel, except: [def: 2]
+          import ElixirLogflow, only: [def: 2]
+        end
     end
   end
 end
--- test/elixir_logflow_test.exs
+++ test/elixir_logflow_test.exs
@@ -9,20 +9,34 @@ defmodule ElixirLogflowTest do
   test "do_def" do
     call_ast = quote(unquote: false) do say_hello end
     body_ast = quote(unquote: false) do [do: :ok] end
-    expected_ast =
-    {{:., [], [{:__aliases__, [alias: false], [:Kernel]}, :def]}, [],
-            [{:say_hello, [], ElixirLogflowTest},
-             [do: {:__block__, [],
-               [{{:., [], [{:__aliases__, [alias: false], [:IO]}, :puts]}, [],
-                 [{:<<>>, [],
-                   ["Calling ",
-                    {:::, [],
-                     [{{:., [], [Kernel, :to_string]}, [],
-                       [{:inspect, [context: ElixirLogflow, import: Kernel],
-                         [{:{}, [], [:say_hello, [], ElixirLogflowTest]}]}]},
-                      {:binary, [], ElixirLogflow}]}]}]}, :ok]}]]}
+    expected_ast = quote context: ElixirLogflow do
+      Kernel.def unquote({:say_hello, [], ElixirLogflowTest}) do
+        do_log(unquote({:{}, [], [:say_hello, [], ElixirLogflowTest]}))
+        :ok
+      end
+    end
 
     result_ast = ElixirLogflow.do_def(call_ast, body_ast)
     assert ^expected_ast = result_ast
+  end
+
+  test "do_using with override" do
+    result_ast = ElixirLogflow.do_using(quote do [skip_log: true] end)
+    assert nil == result_ast
+  end
+
+  test "do_using without override" do
+    expected_ast = quote context: ElixirLogflow do
+      import Kernel, except: [def: 2]
+      import ElixirLogflow, only: [def: 2]
+    end
+    result_ast = ElixirLogflow.do_using(quote do [skip_log: false] end)
+    assert ^expected_ast = result_ast
+  end
+
+
+  test "env override" do
+    call_ast = quote(unquote: false) do say_hello end
+    body_ast = quote(unquote: false) do [do: :ok] end
   end
 end
