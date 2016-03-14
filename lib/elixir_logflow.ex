0cb07fb74c522b360f3459a72dfbe2b239dd78f8
eyal lapid <elpd.dev@gmail.com>
2016-03-14 17:50:50 +0200

    initial working module def macro

--- lib/elixir_logflow.ex
+++ lib/elixir_logflow.ex
@@ -1,2 +1,21 @@
 defmodule ElixirLogflow do
+  defmacro __using__(_) do
+    quote do
+      import Kernel, except: [def: 2]
+      import ElixirLogflow
+    end
+  end
+
+  defmacro def(fun_def_ast, opts_ast) do
+    do_def(fun_def_ast, opts_ast)
+  end
+
+  def do_def(fun_def_ast, opts_ast) do
+    quote do
+      Kernel.def unquote(fun_def_ast) do
+        IO.puts "Calling #{inspect unquote(Macro.escape(fun_def_ast))}"
+        unquote((opts_ast[:do]))
+      end
+    end
+  end
 end
--- test/elixir_logflow_test.exs
+++ test/elixir_logflow_test.exs
@@ -5,4 +5,24 @@ defmodule ElixirLogflowTest do
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  test "do_def" do
+    call_ast = quote(unquote: false) do say_hello end
+    body_ast = quote(unquote: false) do [do: :ok] end
+    expected_ast =
+    {{:., [], [{:__aliases__, [alias: false], [:Kernel]}, :def]}, [],
+            [{:say_hello, [], ElixirLogflowTest},
+             [do: {:__block__, [],
+               [{{:., [], [{:__aliases__, [alias: false], [:IO]}, :puts]}, [],
+                 [{:<<>>, [],
+                   ["Calling ",
+                    {:::, [],
+                     [{{:., [], [Kernel, :to_string]}, [],
+                       [{:inspect, [context: ElixirLogflow, import: Kernel],
+                         [{:{}, [], [:say_hello, [], ElixirLogflowTest]}]}]},
+                      {:binary, [], ElixirLogflow}]}]}]}, :ok]}]]}
+
+    result_ast = ElixirLogflow.do_def(call_ast, body_ast)
+    assert ^expected_ast = result_ast
+  end
 end
