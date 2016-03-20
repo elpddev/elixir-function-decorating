cebb0198e775b39bb669b7395ef8b18b02349d7c
eyal lapid <elpd.dev@gmail.com>
2016-03-20 17:55:50 +0200

    add working test for mock decorator

--- test/elixir_logflow_test.exs
+++ test/elixir_logflow_test.exs
@@ -27,8 +27,15 @@ defmodule ElixirLogflowTest do
     fn_options_ast = [do: quote do word end]
 
     defmodule TestModuleDecorator do
-      def decorate(%FnDef{} = fn_def, decorators) do
-        {:ok, fn_def}
+      def decorate(%FnDef{fn_call_ast: in_fn_call_ast,
+        fn_options_ast: [do: in_do_block] = in_fn_options_ast} = fn_def, decorators) do
+        {:ok, %FnDef{
+          fn_call_ast: in_fn_call_ast,
+          fn_options_ast: [do: quote do
+            decorate_1
+            unquote(in_do_block)
+          end]
+          }}
       end
     end
 
@@ -36,33 +43,14 @@ defmodule ElixirLogflowTest do
         fn_call_ast: fn_call_ast, fn_options_ast: fn_options_ast},
         [TestModuleDecorator])
 
+    decorating_exp = quote context: TestModuleDecorator do decorate_1 end
+
     assert result == {:ok, %FnDef{
         fn_call_ast: quote do beep(word) end,
-        fn_options_ast: [do: quote do word end]
+        fn_options_ast: [do: quote do
+          unquote(decorating_exp)
+          word
+        end]
       }}
-  end
-
-  def tttt do
-    call_ast = quote(unquote: false) do say_hello end
-    body_ast = quote(unquote: false) do [do: :ok] end
-    fun_name = ""
-    expected_ast = quote context: ElixirLogflow do
-      Kernel.def unquote({:say_hello, [], ElixirLogflowTest}) do
-        module = __ENV__.module
-        function_name = unquote(fun_name)
-
-        ElixirLogflow.do_log_pre(module,
-          unquote({:{}, [], [:say_hello, [], ElixirLogflowTest]}))
-        result = :ok
-        ElixirLogflow.do_log_pst(module,
-          unquote({:{}, [], [:say_hello, [], ElixirLogflowTest]}),
-          result)
-
-        result
-      end
-    end
-
-    result_ast = ElixirLogflow.do_def(call_ast, body_ast)
-    assert ^expected_ast = result_ast
   end
 end
